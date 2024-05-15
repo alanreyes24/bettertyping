82afb25ab4d12621fc2ac0b503ee5013b9dfccaf
Alan Reyes <alanreyes6747@gmail.com>
2024-05-15 13:39:12 -0500

    Change timer to be decimal

--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -4,34 +4,22 @@ function Timer({ time, start, stop, onTimerZero, passTimeLeft }) {
     const [timeLeft, setTimeLeft] = useState(time);
 
     useEffect(() => {
-        // console.log(start);
-
         if (start && timeLeft > 0) {
-
-            setTimeout(() => setTimeLeft(timeLeft - 1), 100);
+            setTimeout(() => setTimeLeft(timeLeft - 0.1), 100);
         }
-        if (timeLeft == 0) {
-            onTimerZero()
-            // console.log(onTimerZero)
+        if (timeLeft === 0) {
+            onTimerZero();
         }
 
         if (!start) {
-            console.log('should pass time left here')
-            console.log(timeLeft)
-            passTimeLeft(timeLeft)
+            console.log('should pass time left here');
+            console.log(timeLeft);
+            passTimeLeft(timeLeft);
         }
+    }, [start, stop, timeLeft]);
 
-
-
-    }, [start, stop, timeLeft,]);
-
-
-
-    //MAKE THIS HAVE DECIMAL instead of "300" ie 30.0
-    return <div>{timeLeft}</div>;
+    // Display timeLeft as a floating-point number
+    return <div>{parseFloat(timeLeft).toFixed(1)}</div>;
 }
 
-
 export default Timer;
-
-
