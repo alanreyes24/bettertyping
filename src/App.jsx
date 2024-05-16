440bef4da1ed59613f368bdb6203b3bbac95677d
Alan Reyes <alanreyes6747@gmail.com>
2024-05-16 00:00:17 -0500

    Change variable and function names to be more understandable

--- src/App.jsx
+++ src/App.jsx
@@ -6,44 +6,52 @@ import TextArea from "./components/textarea/TextArea";
 import { React, useState, useEffect } from "react";
 import Cursor from "./components/cursor/Cursor";
 
-function App() {
-  const [startTimer, setStartTimer] = useState(false);
-  const [stopTimer, setStopTimer] = useState(false);
-  const [timerLength, setTimerLength] = useState(30);
-  const [timerZero, setTimerZero] = useState(false);
-  const [isTextFinished, setIsTextFinished] = useState(false);
-  const [correctLetters, setCorrectLetters] = useState(0);
-  const [correctWords, setCorrectWords] = useState(0);
+function App() { // organized all of the states to be cleaner and follow conventions
+
+
+  // condensed "startTimer" and "stopTimer" to be one state "isTimerActive"
+
+  const [isTimerActive, setIsTimerActive] = useState(false)
+
+  const [isTimerZero, setIsTimerZero] = useState(false); // changed "isTimerZero" to be "isTimerZero"
+  const [isTextFinished, setIsTextFinished] = useState(false); // this was named correctly good job
+
+  const [numOfCorrectLetters, setNumOfCorrectLetters] = useState(0); // changed from "correctLetters" to "numOfCorrectLetters"
+  const [numOfCorrectWords, setNumOfCorrectWords] = useState(0); // changed from "correctWords" to "numOfCorrectWords"
+
+  const [timerLength, setTimerLength] = useState(30); // going to need you to explain the relationship between "timerLength" and "timeLeft" more
   const [timeLeft, setTimeLeft] = useState(timerLength);
-  const [WPM, setWPM] = useState(0);
+
+  const [currentTestWPM, setCurrentTestWPM] = useState(0); // changed from "WPM" to "currentTestWPM"
 
   // const getCorrect = (total) => {
   //   console.log("TOTAL" + total)
   // }
 
   useEffect(() => {
-    setWPM((60 * correctWords) / (timerLength - timeLeft))
-  }, [WPM, correctWords, timerLength, timeLeft]);
+    setCurrentTestWPM((60 * numOfCorrectWords) / (timerLength - timeLeft)) // i'm just not going to touch this
+  }, [currentTestWPM, numOfCorrectWords, timerLength, timeLeft]);
 
   const startTest = () => {
     console.log("starting test");
+    console.log(timerLength)
     setTimerLength(timerLength);
-    setTimerZero(false);
-    setStartTimer(true);
+    setIsTimerZero(false);
+    setIsTimerActive(true);
   };
 
   const stopTest = () => {
 
-    setStopTimer(true)
+    setIsTimerActive(true)
     //timeleft is still zero until timer sends it (obviously) it takes a second to come in
     // console.log('TIME LEFT' + timeLeft) doesnt work its still 0
 
-    setStartTimer(false);
+    setIsTimerActive(false);
 
 
   };
 
-  if (timerZero) {
+  if (isTimerZero) {
     stopTest();
   }
 
@@ -59,16 +67,16 @@ function App() {
 
       <button
         onClick={() => {
-          setStartTimer(false);
-          console.log("set timer to 15");
+          setIsTimerActive(false);
           setTimerLength(15);
+          console.log(timerLength)
         }}
       >
         15
       </button>
       <button
         onClick={() => {
-          setStartTimer(false);
+          setIsTimerActive(false);
           setTimerLength(30);
         }}
       >
@@ -76,7 +84,7 @@ function App() {
       </button>
       <button
         onClick={() => {
-          setStartTimer(false);
+          setIsTimerActive(false);
           setTimerLength(60);
         }}
       >
@@ -100,24 +108,24 @@ function App() {
       </button>
       <Timer
         time={timerLength}
-        start={startTimer}
-        stop={stopTimer}
+        start={isTimerActive}
+        stop={isTimerActive}
         onTimerZero={() => {
-          setTimerZero(!timerZero);
+          setIsTimerZero(!isTimerZero);
         }}
-        passTimeLeft={setTimeLeft}
+        changeTimerOption={setTimeLeft}
       />
 
       {isTextFinished ? <>FINISHED YES</> : <>FINISHED NO</>}
-      {" LETTERS " + correctLetters}
-      {" WORDS " + correctWords}
-      {WPM < Infinity ? <>{"WPM: " + WPM}</> : <>{"WPM: 0"}</>}
+      {" LETTERS " + numOfCorrectLetters}
+      {" WORDS " + numOfCorrectWords}
+      {currentTestWPM < Infinity ? <>{"WPM: " + currentTestWPM}</> : <>{"WPM: 0"}</>}
       {/* <Cursor /> */}
       <TextArea
-        passCorrectLetters={setCorrectLetters}
-        passCorrectWords={setCorrectWords}
+        passCorrectLetters={setNumOfCorrectLetters}
+        passCorrectWords={setNumOfCorrectWords}
         onTextStarted={() => {
-          setStartTimer(true)
+          setIsTimerActive(true)
         }}
         onTextFinished={() => {
           setIsTextFinished(!isTextFinished)
--- src/components/timer/Timer.jsx
+++ src/components/timer/Timer.jsx
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from "react";
 
-function Timer({ time, start, stop, onTimerZero, passTimeLeft }) {
-    const [timeLeft, setTimeLeft] = useState(time);
-
+function Timer({ time, start, stop, onTimerZero, changeTimerOption }) {
+    
+    const [timeLeft, setTimeLeft] = useState(time);    
     useEffect(() => {
+
+        changeTimerOption(timeLeft)
+
+        if (!start) {
+
+            changeTimerOption(timeLeft);
+        }
+
         if (start && timeLeft > 0) {
             setTimeout(() => setTimeLeft(timeLeft - 0.1), 100);
         }
+
         if (timeLeft === 0) {
             onTimerZero();
         }
 
-        if (!start) {
-            console.log('should pass time left here');
-            console.log(timeLeft);
-            passTimeLeft(timeLeft);
-        }
     }, [start, stop, timeLeft]);
 
     // Display timeLeft as a floating-point number
