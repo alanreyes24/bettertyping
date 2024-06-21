5a9cc73e21165ea8b5c0d904ac63acb27690b3ba
Alan Reyes <alanreyes6747@gmail.com>
2024-06-21 13:57:40 -0500

    Add previous graph display to History page

--- src/screens/history/History.jsx
+++ src/screens/history/History.jsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { Scatter } from "react-chartjs-2";
 import "./History.css";
 
 function History({ user }) {
   const [allUserTests, setAllUserTests] = useState([]);
-  const [userHasTakenTests, setUserHasTakenTests] = useState(true); // in future make it so that the user can't even click to this page unless they've taken a test perhaps
+  const [userHasTakenTests, setUserHasTakenTests] = useState(true);
+  const [loading, setLoading] = useState(true);
+  const [currentlySelectedTest, setCurrentlySelectedTest] = useState(null);
 
-  const [currentlySelectedTest, setCurrentlySelectedTest] = useState(null); // i guess null works
+  const [trueWPMArray, setTrueWPMArray] = useState([]);
+  const [rawWPMArray, setRawWPMArray] = useState([]);
 
   async function retrieveAllTestsByUser() {
     try {
@@ -16,12 +20,21 @@ function History({ user }) {
 
       setAllUserTests(response.data);
       setCurrentlySelectedTest(response.data[0]);
+
+      setTrueWPMArray(response.data[0].words.trueWPMArray);
+      setRawWPMArray(response.data[0].words.rawWPMArray);
+
+      setLoading(false); // Set loading to false after data is fetched
     } catch (error) {
       console.log(error);
 
-      if (error.response.data === "No tests found for this user.") {
+      if (
+        error.response &&
+        error.response.data === "No tests found for this user."
+      ) {
         setUserHasTakenTests(false);
       }
+      setLoading(false);
     }
   }
 
@@ -29,26 +42,97 @@ function History({ user }) {
     retrieveAllTestsByUser();
   }, []);
 
+  if (loading) {
+    // change this to just be apart of the page instead of the only thing on the page
+    return <div>Loading...</div>; // Render a loading indicator while waiting for data
+  }
+
+  const wpmData = {
+    datasets: [
+      {
+        label: "true wpm",
+        data: trueWPMArray,
+        cubicInterpolationMode: "monotone",
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        showLine: true,
+        fill: false,
+        borderWidth: 1,
+        borderColor: "rgba(255, 255, 255, 1)",
+        pointBackgroundColor: "rgba(255, 255, 255, 1)",
+        pointBorderColor: "#000",
+        pointHoverBackgroundColor: "#000",
+        pointHoverBorderColor: "rgba(255, 255, 255, 1)",
+      },
+      {
+        label: "raw wpm",
+        data: rawWPMArray,
+        cubicInterpolationMode: "monotone",
+        showLine: true,
+        fill: true,
+        borderWidth: 1,
+        backgroundColor: "rgba(0, 0, 0, 0.2)",
+        borderColor: "rgba(0, 0, 0, 1)",
+        pointBackgroundColor: "rgba(0, 0, 0, 1)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(0, 0, 0, 1)",
+      },
+    ],
+  };
+
+  const options = {
+    animation: false,
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "",
+      },
+    },
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        type: "linear",
+        ticks: {
+          stepSize: 1,
+        },
+      },
+      y: {
+        type: "linear",
+      },
+    },
+  };
+
   return (
     <>
       {!userHasTakenTests ? (
-        <div> You need to take a test in order to use this page </div>
+        <div>You need to take a test in order to use this page</div>
       ) : null}
 
       <div className="history-container">
         <div className="test-display">
-          <div> trueWPM: {currentlySelectedTest.results.trueWPM} </div>
-          <div> test length: {currentlySelectedTest.settings.length / 10} </div>
+          <Scatter data={wpmData} options={options} />
         </div>
 
         <div className="history-content">
           {allUserTests.map((test, index) => (
-            <div key={index} className="card">
-              <div> type: {test.settings.type} </div>
-              <div> length: {test.settings.length / 10} </div>
-              <div> trueWPM: {test.results.trueWPM} </div>
-              <div> accuracy: {test.results.accuracy} </div>
-            </div>
+            <button
+              key={index}
+              className="card"
+              onClick={() => {
+                setCurrentlySelectedTest(allUserTests[index]);
+                setTrueWPMArray(allUserTests[index].words.trueWPMArray);
+                setRawWPMArray(allUserTests[index].words.rawWPMArray);
+              }}
+            >
+              <div>Type: {test.settings.type}</div>
+              <div>Length: {(test.settings.length || 0) / 10}</div>
+              <div>True WPM: {test.results.trueWPM}</div>
+              <div>Accuracy: {test.results.accuracy}%</div>
+            </button>
           ))}
         </div>
       </div>
--- src/screens/testfinished/TestFinished.jsx
+++ src/screens/testfinished/TestFinished.jsx
@@ -2,126 +2,125 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { Scatter } from "react-chartjs-2";
 import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Filler,
-    Tooltip,
-    Legend,
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Filler,
+  Tooltip,
+  Legend,
 } from "chart.js";
 
 ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Filler,
-    Title,
-    Tooltip,
-    Legend
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Title,
+  Tooltip,
+  Legend
 );
 
 function TestFinished() {
-    const [mostRecentTest, setMostRecentTest] = useState(null);
-    const [trueWPMArray, setTrueWPMArray] = useState([]);
-    const [rawWPMArray, setRawWPMArray] = useState([]);
+  const [mostRecentTest, setMostRecentTest] = useState(null);
+  const [trueWPMArray, setTrueWPMArray] = useState([]);
+  const [rawWPMArray, setRawWPMArray] = useState([]);
 
-    const retrieveMostRecentChartData = async () => {
-        const response = await axios.get("http://localhost:3090/test/chartData", {
-            withCredentials: true,
-        });
-        setMostRecentTest(response.data);
+  const retrieveMostRecentChartData = async () => {
+    const response = await axios.get("http://localhost:3090/test/chartData", {
+      withCredentials: true,
+    });
+    setMostRecentTest(response.data);
+  };
+
+  useEffect(() => {
+    const fetchDataAndLog = async () => {
+      await retrieveMostRecentChartData();
     };
 
-    
-    useEffect(() => {
-        const fetchDataAndLog = async () => {
-            await retrieveMostRecentChartData();
-        };
+    fetchDataAndLog();
+  }, []);
 
-        fetchDataAndLog();
-    }, []);
+  useEffect(() => {
+    // not really necessary but good to have just in case
+    if (mostRecentTest) {
+      setTrueWPMArray(mostRecentTest.words.trueWPMArray);
+      setRawWPMArray(mostRecentTest.words.rawWPMArray);
+    }
+  }, [mostRecentTest]);
 
-    useEffect(() => { // not really necessary but good to have just in case
-        if (mostRecentTest) {
-            setTrueWPMArray(mostRecentTest.words.trueWPMArray);
-            setRawWPMArray(mostRecentTest.words.rawWPMArray);
-        }
-    }, [mostRecentTest]);
+  const wpmData = {
+    datasets: [
+      {
+        label: "true wpm",
+        data: trueWPMArray,
+        cubicInterpolationMode: "monotone",
+        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        showLine: true,
+        fill: false,
+        borderWidth: 1,
+        borderColor: "rgba(255, 255, 255, 1)",
+        pointBackgroundColor: "rgba(255, 255, 255, 1)",
+        pointBorderColor: "#000",
+        pointHoverBackgroundColor: "#000",
+        pointHoverBorderColor: "rgba(255, 255, 255, 1)",
+      },
+      {
+        label: "raw wpm",
+        data: rawWPMArray,
+        cubicInterpolationMode: "monotone",
+        showLine: true,
+        fill: true,
+        borderWidth: 1,
+        backgroundColor: "rgba(0, 0, 0, 0.2)",
+        borderColor: "rgba(0, 0, 0, 1)",
+        pointBackgroundColor: "rgba(0, 0, 0, 1)",
+        pointBorderColor: "#fff",
+        pointHoverBackgroundColor: "#fff",
+        pointHoverBorderColor: "rgba(0, 0, 0, 1)",
+      },
+    ],
+  };
 
-    const wpmData = {
-        datasets: [
-            {
-                label: "true wpm",
-                data: trueWPMArray,
-                cubicInterpolationMode: "monotone",
-                backgroundColor: "rgba(255, 255, 255, 0.2)",
-                showLine: true,
-                fill: false,
-                borderWidth: 1,
-                borderColor: "rgba(255, 255, 255, 1)",
-                pointBackgroundColor: "rgba(255, 255, 255, 1)",
-                pointBorderColor: "#000",
-                pointHoverBackgroundColor: "#000",
-                pointHoverBorderColor: "rgba(255, 255, 255, 1)",
-            },
-            {
-                label: "raw wpm",
-                data: rawWPMArray,
-                cubicInterpolationMode: "monotone",
-                showLine: true,
-                fill: true,
-                borderWidth: 1,
-                backgroundColor: "rgba(0, 0, 0, 0.2)",
-                borderColor: "rgba(0, 0, 0, 1)",
-                pointBackgroundColor: "rgba(0, 0, 0, 1)",
-                pointBorderColor: "#fff",
-                pointHoverBackgroundColor: "#fff",
-                pointHoverBorderColor: "rgba(0, 0, 0, 1)",
-            },
-        ],
-    };
-
-    const options = {
-        animation: false,
-        responsive: true,
-        plugins: {
-            legend: {
-                position: "top",
-            },
-            title: {
-                display: true,
-                text: "",
-            },
+  const options = {
+    animation: false,
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: true,
+        text: "",
+      },
+    },
+    maintainAspectRatio: false,
+    scales: {
+      x: {
+        type: "linear",
+        ticks: {
+          stepSize: 1,
         },
-        maintainAspectRatio: false,
-        scales: {
-            x: {
-                type: "linear",
-                ticks: {
-                    stepSize: 1,
-                },
-            },
-            y: {
-                type: "linear",
-            },
-        },
-    };
+      },
+      y: {
+        type: "linear",
+      },
+    },
+  };
 
-    return (
-        <>
- 
-            <div>{mostRecentTest?.username}</div>        
-            <div>{mostRecentTest?.results.rawWPM}</div>
-            <div>{mostRecentTest?.results.trueWPM}</div>
-            <div style={{ width: '95vw', height: '50vh' }}>
-                <Scatter data={wpmData} options={options} />
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div>{mostRecentTest?.username}</div>
+      <div>{mostRecentTest?.results.rawWPM}</div>
+      <div>{mostRecentTest?.results.trueWPM}</div>
+      <div style={{ width: "95vw", height: "50vh" }}>
+        <Scatter data={wpmData} options={options} />
+      </div>
+    </>
+  );
 }
 
 export default TestFinished;
