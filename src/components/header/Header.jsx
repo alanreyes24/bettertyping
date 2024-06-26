ed637cf6806aa91e1930bdc7a279710f6802c963
Alan Reyes <alanreyes6747@gmail.com>
2024-06-25 22:54:09 -0500

    Add analysis option to header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -16,7 +16,11 @@ function Header({ username, passLoggedIn, passLogout }) {
 
   async function logUserOut() {
     try {
-      await axios.post('http://localhost:3090/auth/logout', {}, { withCredentials: true });
+      await axios.post(
+        "http://localhost:3090/auth/logout",
+        {},
+        { withCredentials: true }
+      );
     } catch (error) {
       console.log(error);
     }
@@ -28,8 +32,12 @@ function Header({ username, passLoggedIn, passLogout }) {
         <Link to="/">type.ac</Link>
       </div>
 
-      {appUsername === 'guest' ? (
-        <div style={{ fontSize: 'clamp(1rem, 2vw, 1.5rem)', fontWeight: 'bold' }}>guest mode</div>
+      {appUsername === "guest" ? (
+        <div
+          style={{ fontSize: "clamp(1rem, 2vw, 1.5rem)", fontWeight: "bold" }}
+        >
+          guest mode
+        </div>
       ) : appUsername ? (
         <div>welcome, {appUsername}</div>
       ) : null}
@@ -37,22 +45,73 @@ function Header({ username, passLoggedIn, passLogout }) {
       <div className="button-container">
         <Link to="/">
           <button className="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 1024 1024" strokeWidth="0" fill="currentColor" stroke="currentColor" className="icon">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="1em"
+              height="1em"
+              viewBox="0 0 1024 1024"
+              strokeWidth="0"
+              fill="currentColor"
+              stroke="currentColor"
+              className="icon"
+            >
               <path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path>
             </svg>
           </button>
         </Link>
         <Link to="/leaderboard">
           <button className="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-award">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-award"
+            >
               <circle cx="12" cy="8" r="7"></circle>
               <polyline points="8.21 13.89 7 23 12 20 17 23 15.79 13.88"></polyline>
             </svg>
           </button>
         </Link>
+        <Link to="/analysis">
+          <button className="button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="feather feather-bar-chart-2"
+            >
+              <line x1="18" y1="20" x2="18" y2="10"></line>
+              <line x1="12" y1="20" x2="12" y2="4"></line>
+              <line x1="6" y1="20" x2="6" y2="14"></line>
+            </svg>
+          </button>
+        </Link>
         <Link to="/history">
           <button className="button">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-file-text">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-file-text"
+            >
               <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
               <polyline points="14 2 14 8 20 8"></polyline>
               <line x1="16" y1="13" x2="8" y2="13"></line>
@@ -64,18 +123,43 @@ function Header({ username, passLoggedIn, passLogout }) {
 
         {!userLoggedIn ? (
           <button className="button" onClick={() => setShowLogin(!showLogin)}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-log-in">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-log-in"
+            >
               <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
               <polyline points="10 17 15 12 10 7"></polyline>
               <line x1="15" y1="12" x2="3" y2="12"></line>
             </svg>
           </button>
         ) : (
-          <button className="button" onClick={() => {
-            logUserOut();
-            passLogout();
-          }}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-log-out">
+          <button
+            className="button"
+            onClick={() => {
+              logUserOut();
+              passLogout();
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="feather feather-log-out"
+            >
               <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
               <polyline points="16 17 21 12 16 7"></polyline>
               <line x1="21" y1="12" x2="9" y2="12"></line>
