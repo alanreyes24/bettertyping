d2c3f4bf4607d7f34863ccdf60bfe321c7bd9e73
Alan Reyes <alanreyes6747@gmail.com>
2024-10-04 18:45:45 -0500

    Add link to leaderboard button to go to leaderboard page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -72,9 +72,11 @@ function Header({ user, AIMode, passLoggedIn, passLogout }) {
       ) : null}
 
       <div className="flex items-center gap-4">
-        <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
-          Leaderboard
-        </button>
+        <Link to="/leaderboard">
+          <button className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 border border-input bg-background hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2">
+            Leaderboard
+          </button>
+        </Link>
 
         {!userLoggedIn && (
           <button
