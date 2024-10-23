6d8ac7fd515d84be29d8945b31fe4357135a81c6
Miles Oncken <milesoncken@gmail.com>
2024-10-23 08:41:26 -0500

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 
 import { fontFamily } from "tailwindcss/defaultTheme";
+
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        heading: ["var(--font-heading)", ...fontFamily.sans],
-        body: ["var(--font-body)", ...fontFamily.sans],
+        heading: ["Montserrat", ...fontFamily.sans],
+        body: ["Montserrat", ...fontFamily.sans],
       },
       colors: {
         border: "hsl(var(--border))",
